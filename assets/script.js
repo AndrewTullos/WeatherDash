595eb9a05a8df59e973e8b53c2e6fe4d4ed2ea27
Drew Tullos <tullos.andrew@gmail.com>
2023-09-20 21:19:08 -0500

    Working fetch request

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-const apiKey = '1feffd97de3eba178bd8608881f6c445';
-
 /*
 Search a city - presented with 5 day forcast
 5 day forcast needs:
@@ -18,10 +16,72 @@ At the top need the
 - Wind speed
 
 */
-
 const searchFormEl = document.querySelector('#citySearchForm');
-const searchBtn = document.querySelector('#citybtn')
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#citybtn');
+const apiKey = '1feffd97de3eba178bd8608881f6c445';
+let lat;
+let lon;
 
+// Form gathers city
+//Extrapolate lat and lon
+// Assign values to variable
+// Append to page
+
+// GeoCode Lat and Lon extrapolator
+// fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`)
+
+searchFormEl.addEventListener('submit', function(event) {
+event.preventDefault();
+
+    var city = cityInput.value.trim();
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}limit=5&appid=${apiKey}`)
+    .then(function(response) {
+        if (!response.ok) {
+            console.error('Something went wrong. Error: ' + response.statusText);
+            return;
+        }
+        return response.json();
+    })
+    .then(function(data) {
+        if (data) {
+            displayCity(data, city);
+            console.log(data);
+        }
+    })
+    .catch(function(error) {
+        alert('Unable to find the weather');
+    });
+})
+
+
+/*
+// Current Weather Head
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+.then(function(response) {
+    if (!response.ok) {
+        console.error('Something went wrong. Error: ' + response.statusText);
+        return;
+    }
+    return response.json();
+})
+.then(function(data) {
+    if (data) {
+        displayWeather(data, city);
+        console.log(data);
+    }
+})
+.catch(function(error) {
+    alert('Unable to find the weather');
+});
+*/
+
+function displayCity() {
+    console.log('working')
+}
+
+/*
 var formSubmitHandler = function (event) {
     event.preventDefault();
 
@@ -30,88 +90,35 @@ var formSubmitHandler = function (event) {
     if (!searchInputVal) {
         console.error('You need a search input value!');
         return;
-    } else if (searchInputVal) {
+    } else {
         getCityWeather(searchInputVal);
+    }
+};
 
-    } 
-}
-    
-
-var getCityWeather = function (user) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputVal}&appid=${apiKey}`;
+var getCityWeather = function (city) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     fetch(apiUrl)
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-            displayRepos(data, user);
-        });
+                displayWeather(data, city);
+                console.log(data);
+            });
         } else {
-        alert('Error: ' + response.statusText);
+            alert('Error: ' + response.statusText);
         }
     })
     .catch(function (error) {
-        alert('Unable to connect to GitHub');
+        alert('Unable to find the weather');
     });
 };
 
-// THIS NEEDS FORMATTING!!
-var displayRepos = function (repos, searchTerm) {
-    if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
-}
-
-    repoSearchTerm.textContent = searchTerm;
-
-    for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-    }
-};
-
-// searchFormEl.addEventListener('submit', function(event) {
-//     event.preventDefault(); 
-    
-//     var searchInputVal = document.querySelector('#city-input').value;
-
-//     if (!searchInputVal) {
-//         console.error('You need a search input value!');
-//         return;
-//     }
-
-//     var queryString = './search-results.html?q=' + searchInputVal + '&format=';
-
-//     location.assign(queryString);
-
-//     console.log('Working');
-//     console.log('Search input value:', searchInputVal);
-
-// });
-    
-// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
+searchFormEl.addEventListener('submit', function() {
+    formSubmitHandler();
+    getCityWeather();
+});
+*/
 
 
 // // 5 Day Forcast
