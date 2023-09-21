82e45ba6e2eebbffd5e2ee904c0d816939eb4265
Drew Tullos <tullos.andrew@gmail.com>
2023-09-20 23:15:05 -0500

    api functionality 1 day feature created

--- assets/script.js
+++ assets/script.js
@@ -18,10 +18,17 @@ At the top need the
 */
 const searchFormEl = document.querySelector('#citySearchForm');
 const cityInput = document.querySelector('#city-input');
+const citySelected = document.querySelector('#city-selected');
 const searchBtn = document.querySelector('#citybtn');
+const weatherNow = document.querySelector('#weather-now')
+
 const apiKey = '1feffd97de3eba178bd8608881f6c445';
+let cityTitle;
 let lat;
 let lon;
+let temp;
+let wind;
+let humidity;
 
 // Form gathers city
 //Extrapolate lat and lon
@@ -29,14 +36,15 @@ let lon;
 // Append to page
 
 // GeoCode Lat and Lon extrapolator
-// fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`)
-
 searchFormEl.addEventListener('submit', function(event) {
 event.preventDefault();
+// Clear form here
 
     var city = cityInput.value.trim();
+    console.log("City being searched:", city);  
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}limit=5&appid=${apiKey}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
+
     .then(function(response) {
         if (!response.ok) {
             console.error('Something went wrong. Error: ' + response.statusText);
@@ -46,8 +54,12 @@ event.preventDefault();
     })
     .then(function(data) {
         if (data) {
-            displayCity(data, city);
             console.log(data);
+            lat = data[0].lat;
+            lon = data[0].lon;
+            cityTitle = data[0].name;
+            displayCity(data, city);
+            getCityWeather(data);
         }
     })
     .catch(function(error) {
@@ -56,52 +68,17 @@ event.preventDefault();
 })
 
 
-/*
-// Current Weather Head
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-.then(function(response) {
-    if (!response.ok) {
-        console.error('Something went wrong. Error: ' + response.statusText);
-        return;
-    }
-    return response.json();
-})
-.then(function(data) {
-    if (data) {
-        displayWeather(data, city);
-        console.log(data);
-    }
-})
-.catch(function(error) {
-    alert('Unable to find the weather');
-});
-*/
-
-function displayCity() {
-    console.log('working')
-}
-
-/*
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-
-    var searchInputVal = document.querySelector('#city-input').value.trim();
-
-    if (!searchInputVal) {
-        console.error('You need a search input value!');
-        return;
-    } else {
-        getCityWeather(searchInputVal);
-    }
-};
 
 var getCityWeather = function (city) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     fetch(apiUrl)
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+                temp = data.main.temp;
+                wind = data.wind.speed;
+                humidity = data.main.humidity;
                 displayWeather(data, city);
                 console.log(data);
             });
@@ -114,11 +91,34 @@ var getCityWeather = function (city) {
     });
 };
 
-searchFormEl.addEventListener('submit', function() {
-    formSubmitHandler();
-    getCityWeather();
-});
-*/
+//Creating the card to display current weather at top of document
+function displayCity() {
+    console.log('working')
+    weatherNow.classList.add('card');
+    const childDiv = document.createElement('div');
+    citySelected.classList.add('card-title');
+    citySelected.textContent = cityTitle;  
+
+
+    childDiv.className = 'card-body';
+    childDiv.id = 'mainCityCard';
+
+    weatherNow.appendChild(childDiv);
+}
+
+function displayWeather() {
+    console.log('Working');
+    const secondChildDiv = document.createElement('div');
+    secondChildDiv.classList.add('card-text');
+    let mainCityCard = document.getElementById('mainCityCard');
+    secondChildDiv.innerHTML = `Temperature: ${temp}<br><br>Wind: ${wind}<br><br>Humidity: ${humidity}`;
+    mainCityCard.append(secondChildDiv);
+}
+// searchFormEl.addEventListener('submit', function() {
+//     formSubmitHandler();
+//     getCityWeather();
+// });
+
 
 
 // // 5 Day Forcast
